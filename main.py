572dc23e117630a8765fa47304ddeb7ea448a193
FatemehMhbi <mohebbi.fateme@gmail.com>
2021-02-22 16:14:55 -0500

    modified

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ import sys, os
 
 if __name__ == '__main__':
     fasta_file = sys.argv[1]
+    n_clusters = sys.argv[2]
+    pca_n_components = sys.argv[3]
     seqs_as_mat = fasta_to_mat(fasta_file)
-    labeling = clustring(seqs_as_mat, 2, 4)
+    labeling = clustring(seqs_as_mat, n_clusters, pca_n_components)
     labeling.to_csv(fasta_file.split('.csv')[0] + '_kmeans_labels.csv')
